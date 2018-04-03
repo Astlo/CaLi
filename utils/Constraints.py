3b21d77165b37e85ea670605804fb5b7ce6c4853
Yannis <yannis.le-bars@etu.univ-nantes.fr>
2018-04-03 17:27:14 +0200

    Add is_relation_viable

--- utils/Constraints.py
+++ utils/Constraints.py
@@ -2,7 +2,7 @@ import utils.ODRL as ODRL
 
 
 def is_license_viable(license):
-    if not(license.permissions.isdisjoint(license.obligations) and license.permissions.isdisjoint(license.prohibitions) and license.obligations.isdisjoint(license.prohibitions)):
+    if not(license.permissions.isdisjoint(license.obligations) and license.permissions.isdisjoint(license.prohibitions) and license.obligations.isdisjoint(license.prohibitions)) or is_relation_viable(ODRL.REPRODUCTION, ODRL.CONCURRENTUSE, license):
         return False
     return True
 
@@ -13,3 +13,15 @@ def is_compatibility_viable(license_i, license_j):
     if ODRL.DERIVATIVE_WORKS in license_i.prohibitions:
         return False
     return True
+
+def is_relation_viable(action_i, action_j, licence):
+    if action_i in licence.permissions:
+        if action_j in licence.permissions:
+            return True
+        else:
+            return False
+    elif action_i in licence.obligations:
+        if action_j in licence.obligations:
+            return True
+        else:
+            return False
