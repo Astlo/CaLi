e2e15c97f5dfb52cd2a9bf8f07c741ae3c01185d
Yannis <yannis.le-bars@etu.univ-nantes.fr>
2018-04-05 16:08:39 +0200

    Ajout arbre des actions

--- utils/ODRL.py
+++ utils/ODRL.py
@@ -15,3 +15,72 @@ REPRODUCTION = "Reproduction"
 SHARE_ALIKE = "ShareAlike"
 
 CONCURRENTUSE = "concurrentUse"
+
+USE = 'use'
+
+ARBRE = [{
+    'name' : USE,
+    'includedIn' : [{
+        'name' : REPRODUCE,
+        'includedIn' : [{
+            'name' : REPRODUCTION,
+            'includedIn' : [{
+                'name' : CONCURRENTUSE,
+                'includedIn' : []
+            }, {
+               'name' : DIGITIZE,
+               'includedIn' : []
+            }]
+        }]
+     }, {
+        'name' : MOVE,
+        'includedIn' : [{
+           'name' : DELETE,
+           'includedIn' : []
+        }]
+     }, {
+        'name' : SHARING,
+        'includedIn' : [ARBRE_DERIVE]
+     }, {
+        'name' : DERIVATIVE_WORKS,
+        'includedIn' : [{
+           'name' : DISTRIBUTION,
+           'includedIn' : [{
+              'name' : DISTRIBUTE,
+              'includedIn' : []
+           }]
+        }, ARBRE_DERIVE]
+     }, {
+        'name' : MODIFY,
+        'includedIn' : [{
+           'name' : ANNOTATE,
+           'includedIn' : []
+        }, {
+           'name' : ,
+           'includedIn' : []
+        }]
+     }]
+}, {
+   'name' : TRANSFER,
+   'includedIn' : [{
+      'name' : GIVE,
+      'includedIn' : []
+   }, {
+      'name' : SELL,
+      'includedIn' : []
+   }]
+}]
+
+ARBRE_DERIVE = {
+   'name' : DERIVE,
+   'includedIn' : [{
+      'name' : PRINT,
+      'includedIn' : []
+   }, {
+      'name' : PLAY,
+      'includedIn' : [{
+         'name' : DISPLAY,
+         'includedIn' : []
+      }]
+   }]
+}
