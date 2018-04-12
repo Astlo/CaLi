3d679f66e5f7e11b6ad70eecb862ff4204f791ac
Yannis <yannis.le-bars@etu.univ-nantes.fr>
2018-04-12 11:48:18 +0200

    Correction of constraints

--- utils/Constraints.py
+++ utils/Constraints.py
@@ -2,7 +2,7 @@ import utils.ODRL as ODRL
 
 
 def is_license_viable(license):
-    return (license.permissions.isdisjoint(license.obligations) and license.permissions.isdisjoint(license.prohibitions) and license.obligations.isdisjoint(license.prohibitions) and is_permissions_viable(ODRL.ARBRE, license))
+    return (license.permissions.isdisjoint(license.obligations) and license.permissions.isdisjoint(license.prohibitions) and license.obligations.isdisjoint(license.prohibitions) and is_actions_viable(ODRL.ARBRE, license))
 
 
 def is_compatibility_viable(license_i, license_j):
@@ -13,47 +13,13 @@ def is_compatibility_viable(license_i, license_j):
     return True
 
 
-def is_actions_viable(action_i, action_j, license):
-    if action_i in license.permissions:
-        if action_j in license.permissions:
-            return True
-        else:
-            return False
-    elif action_i in license.obligations:
-        if action_j in license.obligations:
-            return True
-        else:
-            return False
-
-
-def is_permissions_viable(tree, license):
-    boolean = True
-    for key in tree:
-        if key in license.permissions:
-            boolean = boolean and browse_permissions(tree[key], license)
-        else:
-            boolean = boolean and is_permissions_viable(tree[key], license)
-    return boolean
-
-
-def browse_permissions(action, license):
-    boolean = True
-    if action != {}:
-        for key in action:
-            if key not in license.permissions:
-                boolean = False
-            else:
-                boolean = boolean and browse_permissions(action[key], license)
-    return boolean
-
-
-def is_prohibitions_viable(tree, license):
+def is_actions_viable(tree, license):
     boolean = True
     for key in tree:
         if key in license.prohibitions:
             boolean = boolean and browse_prohibitions(tree[key], license)
         else:
-            boolean = boolean and is_prohibitions_viable(tree[key], license)
+            boolean = boolean and is_actions_viable(tree[key], license)
     return boolean
 
 
@@ -61,7 +27,7 @@ def browse_prohibitions(action, license):
     boolean = True
     if action != {}:
         for key in action:
-            if key not in license.prohibitions:
+            if key not in license.prohibitions or key in license.permissions or key in license.obligations:
                 boolean = False
             else:
                 boolean = boolean and browse_prohibitions(action[key], license)
